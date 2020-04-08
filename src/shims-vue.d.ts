64e9ff12a80af9b8a5740f78f771e56da09ba56a
Zeithrold <zeithrold@gmail.com>
2020-04-08 16:01:22 +0800

    Imported Vuetify

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,5 +1,9 @@
-declare module '*.vue' {
-  import Vue from 'vue';
+declare module "*.vue" {
+  import Vue from "vue";
 
   export default Vue;
+}
+
+declare module "vuetify/lib" {
+  import "vuetify/types/lib";
 }
